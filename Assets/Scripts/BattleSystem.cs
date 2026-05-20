e83fd0b5cda25471078882baa481d099b7be7373
Milandro Jr. Tolentino <mj.jr2325@gmail.com>
2026-05-20 23:22:16 +0400

    added/updated battlesystem
    - handles the turns of both the player and the enemy

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class BattleSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public PlayerStats playerStats;
+    public EnemyStats enemyStats;
+    public GameManager gameManager;
     void Start()
     {
         
     }
 
+    public void OnPlayerAttack()
+    {
+        if (gameManager.currentState == GameManager.BattleState.PlayerTurn)
+        {
+            enemyStats.TakeDamage(playerStats.damage);
+            if (enemyStats.health > 0)
+            {
+                gameManager.currentState = GameManager.BattleState.EnemyTurn;
+            }
+        }
+    }
+
+    void OnEnemyAttack()
+    {
+            playerStats.TakeDamage(enemyStats.damage);
+            if (playerStats.health > 0)
+            {
+                gameManager.currentState = GameManager.BattleState.PlayerTurn;
+            }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManager.currentState == GameManager.BattleState.EnemyTurn)
+        {
+            OnEnemyAttack();
+        }
     }
 }
