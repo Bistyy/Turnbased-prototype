ccddd50241bb0efff77b1f08cf81189e62812ab7
Milandro Jr. Tolentino <mj.jr2325@gmail.com>
2026-05-22 02:38:30 +0400

    ui improvements
    - shows whose turn is it.
    - hides appropriate ui when winning/losing or enemy turn

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
@@ -9,14 +9,17 @@ public class BattleSystem : MonoBehaviour
     public PlayerStats playerStats;
     public EnemyStats enemyStats;
     public GameManager gameManager;
+
     private bool isProcessing; // interestingly enough, booleans set their default value to false..
     private bool isBattleOver;
 
     public GameObject resultPanel;
     public TextMeshProUGUI resultText;
+    public TextMeshProUGUI turnText;
+    public Button attackButton;
     void Start()
     {
-        
+        turnText.text = "Player Turn";
     }
     IEnumerator EnemyTurnRoutine()
     {
@@ -35,16 +38,20 @@ public class BattleSystem : MonoBehaviour
             {
                 gameManager.currentState = GameManager.BattleState.EnemyTurn;
             }
+            attackButton.gameObject.SetActive(false);
+            turnText.text = "Enemy Turn";
         }
     }
 
     void OnEnemyAttack()
     {
-            playerStats.TakeDamage(enemyStats.damage);
-            if (playerStats.health > 0)
-            {
-                gameManager.currentState = GameManager.BattleState.PlayerTurn;
-            }
+        playerStats.TakeDamage(enemyStats.damage);
+        if (playerStats.health > 0)
+        {
+            gameManager.currentState = GameManager.BattleState.PlayerTurn;
+        }
+        attackButton.gameObject.SetActive(true);
+        turnText.text = "Player Turn";
     }
 
     // Update is called once per frame
@@ -58,15 +65,19 @@ public class BattleSystem : MonoBehaviour
         if (gameManager.currentState == GameManager.BattleState.Win && !isBattleOver)
         {
             resultPanel.SetActive(true);
+            attackButton.gameObject.SetActive(false);
             resultText.text = "You Win!";
             isBattleOver = true;
+            turnText.gameObject.SetActive(false);
         }
 
         else if (gameManager.currentState == GameManager.BattleState.Lose && !isBattleOver)
         {
             resultPanel.SetActive(true);
+            attackButton.gameObject.SetActive(false);
             resultText.text = "You Lose!";
             isBattleOver = true;
+            turnText.gameObject.SetActive(false);
         }
     }
 
