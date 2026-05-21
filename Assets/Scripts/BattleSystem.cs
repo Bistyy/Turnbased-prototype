a4959a806862d02cd988e44250a29d2baf9e66c0
Milandro Jr. Tolentino <mj.jr2325@gmail.com>
2026-05-21 10:00:33 +0400

    coroutines for battlesystem
    - enemy attack has a delay to prevent instant attacks.

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class BattleSystem : MonoBehaviour
@@ -5,9 +6,17 @@ public class BattleSystem : MonoBehaviour
     public PlayerStats playerStats;
     public EnemyStats enemyStats;
     public GameManager gameManager;
+    private bool isProcessing; // interestingly enough, booleans set their default value to false..
     void Start()
     {
         
+    }
+    IEnumerator EnemyTurnRoutine()
+    {
+        isProcessing = true;
+        yield return new WaitForSeconds(1f);
+        OnEnemyAttack();
+        isProcessing = false; 
     }
 
     public void OnPlayerAttack()
@@ -34,9 +43,9 @@ public class BattleSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameManager.currentState == GameManager.BattleState.EnemyTurn)
+        if (gameManager.currentState == GameManager.BattleState.EnemyTurn && !isProcessing)
         {
-            OnEnemyAttack();
+            StartCoroutine(EnemyTurnRoutine());
         }
     }
 }
