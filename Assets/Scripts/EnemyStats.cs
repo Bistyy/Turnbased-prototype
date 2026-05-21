8ff440257d4ddf8a9cb856bab42f97ea3bb5ca05
Milandro Jr. Tolentino <mj.jr2325@gmail.com>
2026-05-21 10:37:53 +0400

    added healthbars
    - healthbars for both player and enemy.

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyStats : MonoBehaviour
 {
+    [SerializeField] private int maxHealth = 200;
     public int health = 100;
     public int damage = 10;
     public GameManager gameManager;
-    
+    public Slider enemySlider;
     void Start()
     {
-
+        health = maxHealth;
+        enemySlider.maxValue = maxHealth;
+        enemySlider.value = health;
     }
 
     // Update is called once per frame
@@ -20,7 +24,7 @@ public class EnemyStats : MonoBehaviour
     public void TakeDamage(int amount)
     {
         health -= amount;
-
+        enemySlider.value = health;
         if (health <= 0)
         {
             health = 0;
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    [SerializeField] private int maxHealth = 150;
     public int health = 150;
     public int damage = 15;
     public GameManager gameManager;
+    public Slider playerSlider;
     void Start()
     {
-        
+        health = maxHealth;
+        playerSlider.maxValue = maxHealth;
+        playerSlider.value = health;
     }
 
     // Update is called once per frame
@@ -20,7 +24,7 @@ public class PlayerStats : MonoBehaviour
     public void TakeDamage(int amount)
     {
         health -= amount;
-
+        playerSlider.value = health;
         if (health <= 0)
         {
             health = 0;
